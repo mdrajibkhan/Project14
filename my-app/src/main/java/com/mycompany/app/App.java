42d872610aec3a121c97a14dcc46fb89995ba421
mdrajibkhan <68429876+mdrajibkhan@users.noreply.github.com>
2021-02-16 16:16:44 +0600

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Maven Project with every commit!" );
+        System.out.println( "Hello World Maven Project with every commit done!" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
